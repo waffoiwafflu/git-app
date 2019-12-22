0c2b1a988a8578b05bf0cfebdf4350e42e58507c
waffoiwafflu <takoman19@gmail.com>
2019-12-22 17:49:46 +0900

    syuusei

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,11 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
-  # GET /articles
-  # GET /articles.json
   def index
     @articles = Article.all
   end
 
-  # GET /articles/1
-  # GET /articles/1.json
+
   def show
   end
 
